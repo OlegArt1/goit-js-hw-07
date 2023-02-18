e1c300ed92b0320af2bfa3588c2c81fb3dfcf7e1
OlegArt1 <art777oleg@gmail.com>
2023-02-19 00:22:23 +0300

    updated code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,37 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 
-import SimpleLightboxJs from "../node_modules/simplelightbox/dist";
-//import SimpleLightboxCss from "../node_modules/simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.esm.js";
 
-var gallery = new SimpleLightBox({elements: '.gallery a'});
-
-//const Lightbox = new SimpleLightbox('.gallery a',
-//    {
-//        loop: true,
-//        captionType: 'alt',
-//        captionDelay: 250
-//    });
-
+const lightbox = new SimpleLightbox('.gallery a',
+{
+  loop: true,
+  captionSelector: 'img',
+  captionType: 'alt',
+  captionData: galleryItems.description,
+  captionPosition: 'bottom',
+  captionDelay: 250
+});
 const imagesGallery = document.querySelector('.gallery');
 const modalcontainerElem = document.querySelector('.image-modal-overlay');
 const modalContentElem = document.querySelector('.image-modal');
 const modalButtonClose = document.querySelector('.modal-button-close');
 const modalButtonIcon = document.querySelector('.modal-button-icon');
 
-//const newElement = document.createElement("li");
-//newElement.classList.add("gallery__item");
-
-//const newElementLink = document.createElement("a");
-//newElementLink.classList.add("gallery__link");
-
-//imagesGallery.append(newElement);
-//newElement.append(newElementLink);
-
 const imgElemtens = galleryItems.reduce((acum, item) =>
 {
   return (acum += `<li class='gallery__item'><a class='gallery__link' href='#'><img class='gallery__image' src="${item.preview}" alt="${item.description}"/></a></li>`);
-},
-    '');
+}, '');
+
 imagesGallery.insertAdjacentHTML('beforeend', imgElemtens);
 
 imagesGallery.addEventListener('click', (event) =>
@@ -46,8 +36,8 @@ imagesGallery.addEventListener('click', (event) =>
 });
 imagesGallery.addEventListener('keydown', (e) =>
 {
-    if (e.code === 'Escape')
-    {
-      modalcontainerElem.classList.remove('visible');
-    }
+  if (e.code === 'Escape')
+  {
+    modalcontainerElem.classList.remove('visible');
+  }
 });
